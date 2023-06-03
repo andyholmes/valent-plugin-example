ba6cdc89c027d6e6c123b203ed23fa1be996706c
Andy Holmes <andrew.g.r.holmes@gmail.com>
2023-06-02 19:56:17 -0700

    refactor: update API usage

--- src/example-plugin.c
+++ src/example-plugin.c
@@ -5,13 +5,13 @@
 #include <libpeas/peas.h>
 
 #include "example-device-plugin.h"
-#include "example-device-preferences-page.h"
+#include "example-device-preferences-group.h"
 
 
 G_MODULE_EXPORT void
 peas_register_types (PeasObjectModule *module)
 {
   example_device_plugin_register_types (module);
-  example_device_preferences_page_register_types (module);
+  example_device_preferences_group_register_types (module);
 }
 
